745c0cfe914d2d4c08ab02b90f6bb48ca94b1b6e
Milos <milosi@niri.rs>
2023-02-26 12:42:37 +0100

    Initial commit

---
+++ src/main/java/com/milos/loanchallange/LoanChallangeApplication.java
@@ -0,0 +1,14 @@
+package com.milos.loanchallange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanChallangeApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(LoanChallangeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/milos/loanchallange/LoanChallangeApplicationTests.java
@@ -0,0 +1,14 @@
+package com.milos.loanchallange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanChallangeApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
